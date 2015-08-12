05cb7deb17997ad16897c6d8efc620b4137852c6
michael ciccarelli <mikecicc@gmail.com>
2015-08-11 23:02:39 -0400

    adding full screen menu with css animations and maintenance notice

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   console.log('$ is ready');
+  $('#menu-toggler').bind('click', function(e){
+    e.preventDefault();
+
+    $('body').toggleClass('menu-open');
+  });
 });
