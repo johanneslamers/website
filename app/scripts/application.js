71fe14e364843bf4ca490abb6f9739dc3fa124ec
michael ciccarelli <mikecicc@gmail.com>
2015-08-28 17:04:24 -0400

    adding navigation, minor styling and copy edits

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,10 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-  console.log('$ is ready');
   $('#menu-toggler').bind('click', function(e){
     e.preventDefault();
 
     $('body').toggleClass('menu-open');
+  });
+
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
